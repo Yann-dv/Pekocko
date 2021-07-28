311220627e01b72c15ecdf4640ddd0b49e40afd8
Yann Dev <h.yann_pro@yahoo.fr>
2021-07-28 14:45:07 +0200

    Add put, delete, get/:id to sauces controllers

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -15,6 +15,35 @@ exports.createSauce = (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
   };
 
+exports.modifySauce = (req, res, next) => { // route pour la modification de l'objet, en fonction de son _id
+    const sauceObject = req.file ? // on vérifie qu'il existe un req.file et donc un fichier envoyé
+    {// Si on trouve un file, on parse le body, et on l'utilise pour créer une nouvelle image et son url
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+    } : {...req.body}; // si pas de file, on récupère le corps de requête
+      Sauce.updateOne({ _id: req.params.id}, {...sauceObject, _id: req.params.id}) // objet en 1er argument, modif en 2ème avec un _id correspondant
+      .then(() => res.status('200').json({message : 'Sauce modifiée'}))
+      .catch(error => res.status('400').json({ error }))
+    };
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id})
+      .then(() => res.status('200').json({message : 'Sauce supprimée'}))
+      .catch(error => res.status('400').json({ error }))
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => { // route d'accès aux objets selon leur id
+      Sauce.findOne({ _id: req.params.id})
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(404).json({ error }));
+};
+
 exports.getAllSauces =  (req, res, next) => {
   Sauce.find()   // Envoi de toutes les sauces en DB
   .then(sauces => res.status(200).json(sauces))
--- routes/sauce.js
+++ routes/sauce.js
@@ -6,10 +6,10 @@ const multer = require('../middleware/multer-config');
 
 
 router.post('/', auth, multer, sauceCtrl.createSauce);
-/*router.post('/:id/like', auth, )
+//router.post('/:id/like', auth, )
 router.put('/:id', auth, multer, sauceCtrl.modifySauce); 
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);*/
-router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces); //
 
 module.exports = router;
