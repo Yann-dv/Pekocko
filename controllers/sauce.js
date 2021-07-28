4d5ab74f9a8fccabc7a9f353070c20c5b1981d20
Yann Dev <h.yann_pro@yahoo.fr>
2021-07-28 14:24:50 +0200

    Defined getAll an postOne sauces controllers

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,7 +1,21 @@
 const Sauce = require('../models/Sauce');
 const fs = require('fs');
 
-exports.getAllSauces =  (req, res, next) => { // ajout de la route d'appel à l'api en premier argument
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({ // récupération du modèle sauce
+      ...sauceObject,
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` ,
+      likes : 0,
+      dislikes: 0// Avec les données passées en formulaire
+    });
+    sauce.save()
+    .then(() => res.status(201).json({message : `${sauceObject.name} enregistrée dans la base de données`}))
+    .catch(error => res.status(400).json({ error }));
+  };
+
+exports.getAllSauces =  (req, res, next) => {
   Sauce.find()   // Envoi de toutes les sauces en DB
   .then(sauces => res.status(200).json(sauces))
   .catch(error => res.status(400).json({error}));
